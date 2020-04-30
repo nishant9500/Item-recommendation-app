c87c1ceab35bfdc19cc608c4359aaa98d2e961ed
Nishant Kumar <nishant.kumar9500@gmail.com>
2020-04-29 22:45:29 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,17 @@
-import numpy as np
-from flask import Flask, request, jsonify, render_template
-import pickle
+from flask import Flask, render_template, request, url_for
+import os
+import sys
+import socket
+import requests
+import json
+import logging
+#from werkzeug import secure_filename
+import pandas as pd
+#from sklearn.externals import joblib
+
+import maincode.py
+
+data = pd.read_pickle('/templates/16k_apperal_data_preprocessed')
 
 app = Flask(__name__)
 #model = pickle.load(open('model.pkl', 'rb'))
@@ -10,18 +21,24 @@ def home():
     return render_template('index.html')
 
 @app.route('/predict',methods=['POST'])
-def predict():
-    '''
-    For rendering results on HTML GUI
-    '''
-    int_features = [int(x) for x in request.form.values()]
-    final_features = [np.array(int_features)]
-    prediction = model.predict(final_features)
+def predictions(M="bag_of_words_model"):
+    if M == "bag_of_words_model":
+        #loaded_model = joblib.load('model/model.pkl')
+        #probs = loaded_model.predict_proba(user_data)
+        x=2
 
-    output = round(prediction[0], 2)
+        return render_template('index.html', prediction_text='Employee Salary should be $ {}'.format(probs[0]))
+    elif M == "tfidf_model":
+        #loaded_model = joblib.load('model/nn.pkl')
+        #probs = loaded_model.predict_proba(user_data)
+        x=1
 
-    return render_template('index.html', prediction_text='Employee Salary should be $ {}'.format(output))
-
+        return render_template('index.html', prediction_text='Employee Salary should be $ {}'.format(probs[0]))
+    elif M=="Opencv_model":
+        #loaded_model = joblib.load('model/nn.pkl')
+        #probs = loaded_model.predict_proba(user_data)
+        x=0
 
 if __name__ == "__main__":
     app.run(debug=True)
+    print(x)
